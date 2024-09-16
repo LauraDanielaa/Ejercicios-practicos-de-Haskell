1beb28507b6cf3d3456521a2d0da73fbe79c46c1
Travelledfever7 <169104860+Travelledfever7@users.noreply.github.com>
2024-09-16 16:54:42 -0500

    Delete Codes/ejercicios1y2.hs

--- Codes/ejercicios1y2.hs
+++
@@ -1,9 +0,0 @@
---Ejercicios practicos Haskell
---1. Funcion ultimaCifra
-	ultimaCifra :: Integer -> Integer
-	ultimaCifra x = x `mod` 10
-
---2. Funcion primeraCifra
-	primeraCifra :: Integer -> Integer
-	primeraCifra x = read[head(show (abs n)]
-
